5d73bb64525e18af21c69f969422abb1092821d2
Paikar <paykar.909090@gmail.com>
2026-01-22 15:11:51 +0200

    Fix

--- PasswordsGenerator/app.js
+++ PasswordsGenerator/app.js
@@ -6,9 +6,11 @@ passwordEl.placeholder = 'Сгенерировать пароль';
 passwordEl.readOnly = true;
 
 const copyBtn = document.createElement('button');
+copyBtn.className = 'password-button'
 copyBtn.textContent = 'Скопировать';
 
 const generateBtn = document.createElement('button');
+generateBtn.classList ='password-button'
 generateBtn.textContent = 'Сгенерировать';
 
 generateBtn.addEventListener('click', () => {
